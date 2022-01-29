86a218f0e7b4de4efda74d190090807e77b88f7c
dlinhhoangnhung <60937131+dlinhhoangnhung@users.noreply.github.com>
2022-01-29 12:16:40 +0700

    Create README.m

---
+++ README.m
@@ -0,0 +1,12 @@
+register
+login JWT
+reset password 
+
+admin: 
+- CRUD items, categories,
+- Receive notify when customer create order, update shipping status
+
+customer:
+- view/add to cart/order  item
+- receive notify
+- check shipping status
