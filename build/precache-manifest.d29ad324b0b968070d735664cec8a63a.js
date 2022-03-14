7ea6ac7540f7ce362cafe92dc7e1b775645e9736
Torin Nguyen <torinnguyen@gmail.com>
2022-03-15 06:41:44 +0700

    update build

---
+++ build/precache-manifest.d29ad324b0b968070d735664cec8a63a.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "646ac3e1f51ad5d2a242",
+    "url": "./static/css/main.2fceec04.chunk.css"
+  },
+  {
+    "revision": "646ac3e1f51ad5d2a242",
+    "url": "./static/js/main.4d0e9118.chunk.js"
+  },
+  {
+    "revision": "998419c8baa8830d3e0d",
+    "url": "./static/js/runtime~main.f34365d2.js"
+  },
+  {
+    "revision": "7dc91eb8cfecea619bef",
+    "url": "./static/css/2.8dead8ec.chunk.css"
+  },
+  {
+    "revision": "7dc91eb8cfecea619bef",
+    "url": "./static/js/2.7b7c7c16.chunk.js"
+  },
+  {
+    "revision": "1e38e421a7c4eac85ac3",
+    "url": "./static/js/3.8cbc31ab.chunk.js"
+  },
+  {
+    "revision": "d25e4ef3dafa20ad03752d522a3aa13e",
+    "url": "./static/media/dot.d25e4ef3.svg"
+  },
+  {
+    "revision": "6e465dd28630d67f6ca5eb354d4c7dcf",
+    "url": "./static/media/clouds_from_my_neighbor_totoro_by_itsendy_deb7t16-fullview.6e465dd2.jpg"
+  },
+  {
+    "revision": "439097b6559104f0e99710b4762599e2",
+    "url": "./static/media/event&gift.439097b6.svg"
+  },
+  {
+    "revision": "6340cf477b2fd0380ce51ed7cf6ab5ff",
+    "url": "./static/media/search-store.6340cf47.svg"
+  },
+  {
+    "revision": "f7ae7fd6e8f0dda99480baa5d1612d2a",
+    "url": "./static/media/search.f7ae7fd6.svg"
+  },
+  {
+    "revision": "602c7d06a0a1880d04ad9d9de405cc26",
+    "url": "./static/media/search-pack.602c7d06.svg"
+  },
+  {
+    "revision": "6d2cdb8dce4a82657229986e78748778",
+    "url": "./static/media/clothing.6d2cdb8d.svg"
+  },
+  {
+    "revision": "17a0e4acc982c4535855d4f169f6bb9a",
+    "url": "./static/media/new.17a0e4ac.svg"
+  },
+  {
+    "revision": "404f76d310c4df869b98e176ac592d83",
+    "url": "./static/media/accessories.404f76d3.svg"
+  },
+  {
+    "revision": "98f7d427b131d9f4889669cb5cd85292",
+    "url": "./static/media/Star.98f7d427.svg"
+  },
+  {
+    "revision": "89b9588e1ef2135ca5159da5be2928f6",
+    "url": "./static/media/PhoneCase.89b9588e.svg"
+  },
+  {
+    "revision": "6f7d53c5cece64c3fee2b73104434836",
+    "url": "./static/media/ClutchBag.6f7d53c5.svg"
+  },
+  {
+    "revision": "037aebc973dcaf6e07ef43d65adcd540",
+    "url": "./static/media/Tracksuit.037aebc9.svg"
+  },
+  {
+    "revision": "ee4493425c5d1ea4f8ee2015694b79e6",
+    "url": "./static/media/Trousers.ee449342.svg"
+  },
+  {
+    "revision": "0ae5a8313a7f9ce3f8211a685bc0cd03",
+    "url": "./static/media/Jersey.0ae5a831.svg"
+  },
+  {
+    "revision": "d19f5471248fcbf9099120d20bfbcc72",
+    "url": "./static/media/SlipDress.d19f5471.svg"
+  },
+  {
+    "revision": "62e92d279db03e9e38ec2f26dd12c3b2",
+    "url": "./static/media/Skirt.62e92d27.svg"
+  },
+  {
+    "revision": "f75e2c2ce73ab9f8c8aa8a4b3b10d2df",
+    "url": "./static/media/HeartPuzzle.f75e2c2c.svg"
+  },
+  {
+    "revision": "c1792364c31015f85fe9810e7aec2d2e",
+    "url": "./static/media/landing.c1792364.png"
+  },
+  {
+    "revision": "a5a055b2540dfee2c9929dd672152c95",
+    "url": "./static/media/diamond.a5a055b2.svg"
+  },
+  {
+    "revision": "95abf109fb2f4673ff178647eeedfd8b",
+    "url": "./static/media/basket.95abf109.svg"
+  },
+  {
+    "revision": "339c22047b56aa1159ce9cc825e60c66",
+    "url": "./index.html"
+  }
+];
