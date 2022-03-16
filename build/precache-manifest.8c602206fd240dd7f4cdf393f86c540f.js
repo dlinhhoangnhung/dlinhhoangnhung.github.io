70a3e1f86aa67658bb31c108c5b458c0e97046ad
Hoang Nhung <nhungdlinhhoang@gmail.com>
2022-03-16 10:01:54 +0700

    rebuild

---
+++ build/precache-manifest.8c602206fd240dd7f4cdf393f86c540f.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdda2d68578dffd94717",
+    "url": "/react-app/static/css/main.1286b85a.chunk.css"
+  },
+  {
+    "revision": "cdda2d68578dffd94717",
+    "url": "/react-app/static/js/main.47394afd.chunk.js"
+  },
+  {
+    "revision": "7565eb577baea67c27f5",
+    "url": "/react-app/static/js/runtime~main.335a61f2.js"
+  },
+  {
+    "revision": "fb7849ea5057dae66e50",
+    "url": "/react-app/static/css/2.68d42cbc.chunk.css"
+  },
+  {
+    "revision": "fb7849ea5057dae66e50",
+    "url": "/react-app/static/js/2.5c08ca6b.chunk.js"
+  },
+  {
+    "revision": "b671e890b11aaadeb3be",
+    "url": "/react-app/static/js/3.20927385.chunk.js"
+  },
+  {
+    "revision": "d25e4ef3dafa20ad03752d522a3aa13e",
+    "url": "/react-app/static/media/dot.d25e4ef3.svg"
+  },
+  {
+    "revision": "6e465dd28630d67f6ca5eb354d4c7dcf",
+    "url": "/react-app/static/media/clouds_from_my_neighbor_totoro_by_itsendy_deb7t16-fullview.6e465dd2.jpg"
+  },
+  {
+    "revision": "439097b6559104f0e99710b4762599e2",
+    "url": "/react-app/static/media/event&gift.439097b6.svg"
+  },
+  {
+    "revision": "6340cf477b2fd0380ce51ed7cf6ab5ff",
+    "url": "/react-app/static/media/search-store.6340cf47.svg"
+  },
+  {
+    "revision": "f7ae7fd6e8f0dda99480baa5d1612d2a",
+    "url": "/react-app/static/media/search.f7ae7fd6.svg"
+  },
+  {
+    "revision": "602c7d06a0a1880d04ad9d9de405cc26",
+    "url": "/react-app/static/media/search-pack.602c7d06.svg"
+  },
+  {
+    "revision": "6d2cdb8dce4a82657229986e78748778",
+    "url": "/react-app/static/media/clothing.6d2cdb8d.svg"
+  },
+  {
+    "revision": "17a0e4acc982c4535855d4f169f6bb9a",
+    "url": "/react-app/static/media/new.17a0e4ac.svg"
+  },
+  {
+    "revision": "404f76d310c4df869b98e176ac592d83",
+    "url": "/react-app/static/media/accessories.404f76d3.svg"
+  },
+  {
+    "revision": "98f7d427b131d9f4889669cb5cd85292",
+    "url": "/react-app/static/media/Star.98f7d427.svg"
+  },
+  {
+    "revision": "89b9588e1ef2135ca5159da5be2928f6",
+    "url": "/react-app/static/media/PhoneCase.89b9588e.svg"
+  },
+  {
+    "revision": "6f7d53c5cece64c3fee2b73104434836",
+    "url": "/react-app/static/media/ClutchBag.6f7d53c5.svg"
+  },
+  {
+    "revision": "037aebc973dcaf6e07ef43d65adcd540",
+    "url": "/react-app/static/media/Tracksuit.037aebc9.svg"
+  },
+  {
+    "revision": "ee4493425c5d1ea4f8ee2015694b79e6",
+    "url": "/react-app/static/media/Trousers.ee449342.svg"
+  },
+  {
+    "revision": "0ae5a8313a7f9ce3f8211a685bc0cd03",
+    "url": "/react-app/static/media/Jersey.0ae5a831.svg"
+  },
+  {
+    "revision": "d19f5471248fcbf9099120d20bfbcc72",
+    "url": "/react-app/static/media/SlipDress.d19f5471.svg"
+  },
+  {
+    "revision": "62e92d279db03e9e38ec2f26dd12c3b2",
+    "url": "/react-app/static/media/Skirt.62e92d27.svg"
+  },
+  {
+    "revision": "f75e2c2ce73ab9f8c8aa8a4b3b10d2df",
+    "url": "/react-app/static/media/HeartPuzzle.f75e2c2c.svg"
+  },
+  {
+    "revision": "a5a055b2540dfee2c9929dd672152c95",
+    "url": "/react-app/static/media/diamond.a5a055b2.svg"
+  },
+  {
+    "revision": "95abf109fb2f4673ff178647eeedfd8b",
+    "url": "/react-app/static/media/basket.95abf109.svg"
+  },
+  {
+    "revision": "85249d9035035ec3577b52d51e4ce064",
+    "url": "/react-app/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.972b3efee09c7dea1f84f4577a392099.js"
+  "/react-app/precache-manifest.8c602206fd240dd7f4cdf393f86c540f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/react-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
