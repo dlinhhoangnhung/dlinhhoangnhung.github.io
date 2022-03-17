73b9016ee9fe1f761f350a33d5df548e340e2a42
Hoang Nhung <nhungdlinhhoang@gmail.com>
2022-03-17 18:01:19 +0700

    Updates

--- precache-manifest.c2702f97a511fc05191e7aecb84f128b.js
+++ precache-manifest.c2702f97a511fc05191e7aecb84f128b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "3e0eec464a35e3f7a681",
+    "revision": "3f6c661174f5a1710ef6",
     "url": "/static/css/main.ec48ae9d.chunk.css"
   },
   {
-    "revision": "3e0eec464a35e3f7a681",
-    "url": "/static/js/main.ccf944da.chunk.js"
+    "revision": "3f6c661174f5a1710ef6",
+    "url": "/static/js/main.d76d6b47.chunk.js"
   },
   {
     "revision": "c259c2e6768d9578f283",
@@ -104,7 +104,7 @@ self.__precacheManifest = [
     "url": "/static/media/basket.95abf109.svg"
   },
   {
-    "revision": "025fb160b13882d652a8bd35ec6ec713",
+    "revision": "8cebfe396e4847cc415bad76cb98f3ea",
     "url": "/index.html"
   }
 ];
