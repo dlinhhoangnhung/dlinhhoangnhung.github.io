0899ef2a4b1cf2f62d3b1f463c4fe2396734a65e
Hoang Nhung <nhungdlinhhoang@gmail.com>
2022-03-17 15:15:09 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.beb0718f61ba8044e87fb96afab5f29b.js"
+  "/precache-manifest.19e8c367cbb733342e31ca7927e6a6d9.js"
 );
 
 workbox.clientsClaim();
